1b853112d5a2a715882eef15a3ff4de83623fa02
Ismail Bennani <avace.contact@gmail.com>
2023-11-05 21:33:45 +0100

    use

--- PockedeckBattler.Angular/src/app/api/api.module.ts
+++ PockedeckBattler.Angular/src/app/api/api.module.ts
@@ -3,13 +3,14 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ExceptionInterceptor } from './exception-interceptor.service';
 import { API_BASE_URL, CombatsService } from './pockedeck-battler-api-client';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
   providers: [
     CombatsService,
-    { provide: API_BASE_URL, useValue: 'http://localhost:5295' },
+    { provide: API_BASE_URL, useValue: environment.apiUrl },
     { provide: HTTP_INTERCEPTORS, useClass: ExceptionInterceptor, multi: true },
   ],
 })
